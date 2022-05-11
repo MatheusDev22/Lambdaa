493d14356e17ae93c2f96af1d5ca88f6cbed24f5
MatheusDev22 <matheusbsb95@gmail.com>
2022-05-11 16:50:48 -0300

    funcional programming, passing a function as an argument

--- src/entiti/Product.java
+++ src/entiti/Product.java
@@ -2,4 +2,30 @@ package entiti;
 
 public class Product {
 
+	public String name;
+	public Double price;
+	
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	
+	public  String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double Price) {
+		this.price = price;
+	}
+	
+	public String toString() {
+		return "Name: "+ getName() + ", " + "Price: R$ " + getPrice();
+	}
 }
